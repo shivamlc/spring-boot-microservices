56de106bbb50837601d9a17884b40dd3b0d5a054
shivamacshack <s.gaur@uq.edu.au>
2025-04-01 20:42:35 +1000

    added comment

--- src/main/java/com/sg/bank/accounts/AccountsApplication.java
+++ src/main/java/com/sg/bank/accounts/AccountsApplication.java
@@ -3,7 +3,7 @@ package com.sg.bank.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // performs auto configuration; scans all beans in spring boot app
 public class AccountsApplication {
 
 	public static void main(String[] args) {
